874343421eed78e3d7974a0231700f650626dbe5
Bogdan Vasile <vasileeduardbogdan@gmail.com>
2026-01-07 02:02:10 +0200

    Add Railway deployment configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  nitro: {
+    preset: 'node-server'
+  }
 })
 
